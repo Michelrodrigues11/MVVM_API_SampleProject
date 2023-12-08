2aa97b1a29e0da03dfc37383ebcfa91e78332a0c
Michel <henriquemichel10@gmail.com>
2023-12-08 15:58:44 -0300

    Projeto Terminado

---
+++ ViewModels/ToDoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVM_API_SampleProject.Models;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Windows.Input;
+using MVVM_API_SampleProject.Services;
+
+
+namespace MVVM_API_SampleProject.ViewModels;
+
+internal partial class ToDoViewModel : ObservableObject, IDisposable
+{
+    private readonly ToDoServices _todoService;
+
+    [ObservableProperty]
+    public int _UserId;
+    [ObservableProperty]
+    public int _Id;
+    [ObservableProperty]
+    public string _Title;
+    [ObservableProperty]
+    public bool _Completed;
+    [ObservableProperty]
+    public ObservableCollection<ToDo> _todos;
+
+
+    public ICommand GetToDoCommand { get; }
+
+    public ToDoViewModel()
+    {
+        Todos = new ObservableCollection<ToDo>();
+        _todoService = new ToDoServices();
+
+        GetToDoCommand = new Command(async () => await LoadToDoAsync()); //
+        Task.Run(async () => await LoadToDoAsync());
+    }
+
+
+    private async Task LoadToDoAsync()
+    {
+        Todos = await _todoService.GetToDoAsync();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
